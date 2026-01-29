5286b0e8b85cc3928fc4334dc0347dee16aede3a
StudioAlbert <account@studio-albert.com>
2026-01-29 10:23:26 +0100

    add many projects

---
+++ 2 - Courses/UnityCourse-TPSAdventure/Assets/03 - Scripts/PlayerInputs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterInputs : MonoBehaviour
+{
+
+    public Vector2 InputMove;
+
+    public void OnMove(InputAction.CallbackContext ctx) => InputMove = ctx.ReadValue<Vector2>();
+
+    
+}
